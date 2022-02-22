2df63374f7afcc8425d3f70bd24a10f6e783c8a9
naveen098 <78691421+singhnaveen098@users.noreply.github.com>
2022-02-23 00:58:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const cors = require('cors')
 const app = express()
 const connecttomongo = require('./db')
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 connecttomongo()
 
@@ -16,10 +16,6 @@ app.use(express.static(path.resolve(__dirname, '../build')));
 //available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
 
 app.listen(port, () => {
     console.log(`CloudNote backend listening at http://localhost:${port}`)
