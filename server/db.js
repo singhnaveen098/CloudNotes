9d7ff2be1348a23def03f2238cc130c3c6975ef8
naveen098 <78691421+singhnaveen098@users.noreply.github.com>
2022-02-23 00:15:55 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/cloudnotes?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+const mongoURI=process.env.ATLAS
 
-const connecttomongo=()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log('connected to mongo successfully')
-    })
+const connecttomongo =()=>{
+    mongoose.connect(mongoURI,{keepAlive:true},()=>console.log('Connected to mongo succesfully'));
+
 }
 
 module.exports = connecttomongo
