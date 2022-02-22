bec025601b5f8a83e34c4cc0835a3df363c98fcb
naveen098 <78691421+singhnaveen098@users.noreply.github.com>
2022-02-23 00:07:26 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 function NoteState(props) {
-  const host = "http://localhost:3001"
+  const host = ""
   const notesinitial = []
   const [notes, setnotes] = useState(notesinitial)
 
