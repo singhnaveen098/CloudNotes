a2bf5a5d6a96b4d3037848fee2b8d1a89894f0bb
naveen098 <78691421+singhnaveen098@users.noreply.github.com>
2022-02-24 17:04:26 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,39 +17,39 @@ function About() {
                 <span className='text-lg font-bold'><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=singh.naveen098@gmail.com" target="_blank" className='hover:text-black'>singh.naveen098@gmail.com</a></span>
                 <h1 className='text-2xl font-bold my-16'>Languages</h1>
                 <div className='flex flex-wrap justify-between'>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={c} alt="c" className='h-36'/>
                         <h2 className='font-bold'>C</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={cpp} alt="c" className='h-36'/>
                         <h2 className='font-bold'>C++</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={css} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Css 3</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={html} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Html 5</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={java} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Java</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={js} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Javascript</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={nodejs} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Nodejs</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={python} alt="c" className='h-36'/>
                         <h2 className='font-bold'>Python</h2>
                     </div>
-                    <div className='flex-col p-10'>
+                    <div className='flex-col p-10 mx-auto'>
                         <img src={react} alt="c" className='h-36'/>
                         <h2 className='font-bold'>React js/Native</h2>
                     </div>
